3b7e7da4597c1706d2c0608bc766c9090d81a2f5
Michael Howell <michael@notriddle.com>
2016-12-16 13:11:15 -0700

    Use the PEM parser for PEM data

--- web/integration/github.ex
+++ web/integration/github.ex
@@ -18,7 +18,7 @@ defmodule Aelita2.Integration.GitHub do
   def get_installation_token!(installation_xref) do
     import Joken
     cfg = config()
-    pem = JOSE.JWK.from_binary(cfg[:pem])
+    pem = JOSE.JWK.from_pem(cfg[:pem])
     jwt_token = token()
     |> with_iss(cfg[:iss])
     |> sign(rs256(pem))
