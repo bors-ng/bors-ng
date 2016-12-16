5913350cda7ba63ebf79d4d06aa8a0bc6ee5bfb7
Michael Howell <michael@notriddle.com>
2016-12-15 18:11:04 -0700

    Fix syntax

--- web/controllers/webhook_controller.ex
+++ web/controllers/webhook_controller.ex
@@ -15,34 +15,32 @@ defmodule Aelita2.WebhookController do
     payload = Poison.decode!(conn.body)
     installation_id = payload["installation"]["id"]
     case payload["action"] do
-      "deleted" -> Repo.delete_all! from(
+      "deleted" -> Repo.delete_all(from(
         i in Installation,
-        where i.installation_id = installation_id
-      )
-      "created" -> Repo.insert! %Installation{
+        where: i.installation_id == ^installation_id
+      ))
+      "created" -> Repo.insert!(%Installation{
         installation_id: installation_id
-      }
+      })
     end
   end
 
   def do_webhook(conn, "github", "integration_installation_repositories") do
     payload = Poison.decode!(conn.body)
     installation_id = payload["installation"]["id"]
-    installation = Repo.get(
-      from i in Installation, where i.installation_id = installation_id
-    )
+    installation = Repo.get_by!(Installation, installation_id: installation_id)
     :ok = case payload["action"] do
       "removed" -> :ok
       "added" -> :ok
     end
     Enum.each(
       payload["repositories_removed"],
-      fn(r) -> Repo.delete_all! from(
+      fn(r) -> Repo.delete_all(from(
         p in Project,
-        where p.repo_id = r["id"]))
+        where: p.repo_id == ^r["id"])) end)
     Enum.each(
       payload["repositories_added"],
       fn(r) -> Repo.insert! %Project{
-        repo_id: r["id"], name: r["full_name"], installation: installation})
+        repo_id: r["id"], name: r["full_name"], installation: installation} end)
   end
 end
