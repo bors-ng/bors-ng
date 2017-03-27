c72fe9e3090d284b2b7cb0fa09f396536f136163
Michael Howell <michael@notriddle.com>
2017-03-27 10:40:19 -0700

    Bump version to v0.0.4

--- apps/bors_frontend/mix.exs
+++ apps/bors_frontend/mix.exs
@@ -3,7 +3,7 @@ defmodule BorsNG.Mixfile do
 
   def project do
     [ app: :bors_frontend,
-      version: "0.0.3",
+      version: "0.0.4",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/bors_github/mix.exs
+++ apps/bors_github/mix.exs
@@ -3,7 +3,7 @@ defmodule BorsNG.GitHub.Mixfile do
 
   def project do
     [ app: :bors_github,
-      version: "0.0.1",
+      version: "0.0.4",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
