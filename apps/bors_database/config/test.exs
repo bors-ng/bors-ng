3b3968cda63f0cfce762b05c2d83abf8e4a62ef8
Michael Howell <michael@notriddle.com>
2017-04-13 11:15:23 -0700

    Eliminate the last uses of  the old name 👩🔫

--- apps/bors_database/config/test.exs
+++ apps/bors_database/config/test.exs
@@ -4,6 +4,6 @@ config :bors_database, BorsNG.Database.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "Postgres1234",
-  database: "aelita2_test",
+  database: "bors_test",
   hostname: System.get_env("POSTGRES_HOST") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
