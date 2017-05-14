8ce5037990be8c3ef30be1a2f900dd4944c5c3f2
Michael Howell <michael@notriddle.com>
2017-05-14 11:43:31 -0700

    Remove inaccessible aliases
    Too much hassle remembering to use them...

--- apps/bors_database/mix.exs
+++ apps/bors_database/mix.exs
@@ -36,16 +36,8 @@ defmodule BorsNG.Database.Mixfile do
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to create, migrate and run the seeds file at once:
-  #
-  #     $ mix ecto.setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
       "test": ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
