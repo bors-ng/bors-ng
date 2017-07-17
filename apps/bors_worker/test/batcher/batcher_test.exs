bors-ng/bors-ng

commit 1a9c378d9e000f86b02edde385c71f93bc0ac93b

Add the reviewer and PR body to the commit message
Fixes #31
Fixes #82

apps/bors_frontend/lib/bors_ng/batcher.ex
@@ -39,8 +39,8 @@ defmodule BorsNG.Batcher do
     GenServer.start_link(__MODULE__, project_id)
   end
 
-  def reviewed(pid, patch_id) when is_integer(patch_id) do
-    GenServer.cast(pid, {:reviewed, patch_id})
+  def reviewed(pid, patch_id, reviewer) when is_integer(patch_id) do
+    GenServer.cast(pid, {:reviewed, patch_id, reviewer})
   end
 
   def status(pid, stat) do
@@ -67,7 +67,7 @@ defmodule BorsNG.Batcher do
     {:noreply, project_id}
   end
 
-  def do_handle_cast({:reviewed, patch_id}, project_id) do
+  def do_handle_cast({:reviewed, patch_id, reviewer}, project_id) do
     case Repo.get(Patch.all(:awaiting_review), patch_id) do
       nil ->
         # Patch exists (otherwise, no ID), but is not awaiting review
@@ -84,7 +84,10 @@ defmodule BorsNG.Batcher do
         repo_conn = get_repo_conn(project)
         case patch_preflight(repo_conn, patch) do
           :ok ->
-            params = %{batch_id: batch.id, patch_id: patch.id}
+            params = %{
+              batch_id: batch.id,
+              patch_id: patch.id,
+              reviewer: reviewer }
             Repo.insert!(LinkPatchBatch.changeset(%LinkPatchBatch{}, params))
             poll_at = (project.batch_delay_sec + 1) * 1000
             Process.send_after(self(), {:poll, :once}, poll_at)
@@ -200,9 +203,7 @@ defmodule BorsNG.Batcher do
   defp start_waiting_batch(batch) do
     project = batch.project
     repo_conn = get_repo_conn(project)
-    patches = batch.id
-    |> Patch.all_for_batch()
-    |> Repo.all()
+    patch_links = Repo.all(LinkPatchBatch.from_batch(batch.id))
     stmp = "#{project.staging_branch}.tmp"
     base = GitHub.get_branch!(
       repo_conn,
@@ -214,7 +215,7 @@ defmodule BorsNG.Batcher do
         tree: base.tree,
         parents: [base.commit],
         commit_message: "[ci skip]"})
-    do_merge_patch = fn patch, branch ->
+    do_merge_patch = fn %{ patch: patch }, branch ->
       case branch do
         :conflict -> :conflict
         _ -> GitHub.merge_branch!(
@@ -225,8 +226,12 @@ defmodule BorsNG.Batcher do
             commit_message: "[ci skip] -bors-staging-tmp-#{patch.pr_xref}"})
       end
     end
-    merge = Enum.reduce(patches, tbase, do_merge_patch)
-    {status, commit} = start_waiting_merged_batch(batch, patches, base, merge)
+    merge = Enum.reduce(patch_links, tbase, do_merge_patch)
+    {status, commit} = start_waiting_merged_batch(
+      batch,
+      patch_links,
+      base,
+      merge)
     now = DateTime.to_unix(DateTime.utc_now(), :seconds)
     GitHub.delete_branch!(repo_conn, stmp)
     send_status(repo_conn, batch, status)
@@ -238,10 +243,10 @@ defmodule BorsNG.Batcher do
     status
   end
 
-  defp start_waiting_merged_batch(batch, patches, base, %{tree: tree}) do
+  defp start_waiting_merged_batch(batch, patch_links, base, %{tree: tree}) do
     repo_conn = get_repo_conn(batch.project)
-    parents = [base.commit | Enum.map(patches, &(&1.commit))]
-    commit_message = Batcher.Message.generate_commit_message(patches)
+    parents = [base.commit | Enum.map(patch_links, &(&1.patch.commit))]
+    commit_message = Batcher.Message.generate_commit_message(patch_links)
     head = GitHub.synthesize_commit!(
       repo_conn,
       %{
@@ -249,11 +254,13 @@ defmodule BorsNG.Batcher do
         tree: tree,
         parents: parents,
         commit_message: commit_message})
+    patches = Enum.map(patch_links, &(&1.patch))
     {setup_statuses(repo_conn, batch, patches), head}
   end
 
-  defp start_waiting_merged_batch(batch, patches, _base, :conflict) do
+  defp start_waiting_merged_batch(batch, patch_links, _base, :conflict) do
     repo_conn = get_repo_conn(batch.project)
+    patches = Enum.map(patch_links, &(&1.patch))
     state = bisect(patches, batch.project)
     send_message(repo_conn, patches, {:conflict, state})
     {:conflict, nil}

apps/bors_frontend/test/batcher/batcher_test.exs
@@ -128,7 +128,7 @@ defmodule BorsNG.BatcherTest do
     patch = %Patch{project_id: proj.id, pr_xref: 1} |> Repo.insert!()
     batch = %Batch{project_id: proj.id, state: 0} |> Repo.insert!()
     %LinkPatchBatch{patch_id: patch.id, batch_id: batch.id} |> Repo.insert!()
-    Batcher.handle_cast({:reviewed, patch.id}, proj.id)
+    Batcher.handle_cast({:reviewed, patch.id, "rvr"}, proj.id)
     state = GitHub.ServerMock.get_state()
     assert state == %{
       {{:installation, 91}, 14} => %{
@@ -153,7 +153,7 @@ defmodule BorsNG.BatcherTest do
       }})
     patch = %Patch{project_id: proj.id, pr_xref: 1, commit: "Z"}
     |> Repo.insert!()
-    Batcher.handle_cast({:reviewed, patch.id}, proj.id)
+    Batcher.handle_cast({:reviewed, patch.id, "rvr"}, proj.id)
     state = GitHub.ServerMock.get_state()
     assert state == %{
       {{:installation, 91}, 14} => %{
@@ -178,7 +178,7 @@ defmodule BorsNG.BatcherTest do
       }})
     patch = %Patch{project_id: proj.id, pr_xref: 1, commit: "Z"}
     |> Repo.insert!()
-    Batcher.handle_cast({:reviewed, patch.id}, proj.id)
+    Batcher.handle_cast({:reviewed, patch.id, "rvr"}, proj.id)
     state = GitHub.ServerMock.get_state()
     assert state == %{
       {{:installation, 91}, 14} => %{
@@ -205,7 +205,7 @@ defmodule BorsNG.BatcherTest do
       pr_xref: 1,
       commit: "N"}
     |> Repo.insert!()
-    Batcher.handle_cast({:reviewed, patch.id}, proj.id)
+    Batcher.handle_cast({:reviewed, patch.id, "rvr"}, proj.id)
     assert GitHub.ServerMock.get_state() == %{
       {{:installation, 91}, 14} => %{
         branches: %{ "master" => "ini", "staging" => "", "staging.tmp" => "" },
@@ -252,7 +252,7 @@ defmodule BorsNG.BatcherTest do
       pr_xref: 1,
       commit: "N"}
     |> Repo.insert!()
-    Batcher.handle_cast({:reviewed, patch.id}, proj.id)
+    Batcher.handle_cast({:reviewed, patch.id, "rvr"}, proj.id)
     assert GitHub.ServerMock.get_state() == %{
       {{:installation, 91}, 14} => %{
         branches: %{ "master" => "ini", "staging" => "", "staging.tmp" => "" },
@@ -369,7 +369,7 @@ defmodule BorsNG.BatcherTest do
       pr_xref: 2,
       commit: "O"}
     |> Repo.insert!()
-    Batcher.handle_cast({:reviewed, patch.id}, proj.id)
+    Batcher.handle_cast({:reviewed, patch.id, "rvr"}, proj.id)
     assert GitHub.ServerMock.get_state() == %{
       {{:installation, 91}, 14} => %{
         branches: %{ "master" => "ini", "staging" => "", "staging.tmp" => "" },
@@ -397,7 +397,7 @@ defmodule BorsNG.BatcherTest do
         files: %{ "staging" => %{ "bors.toml" => ~s/status = [ "ci" ]/ } }
       }}
     # Submit the second one.
-    Batcher.handle_cast({:reviewed, patch2.id}, proj.id)
+    Batcher.handle_cast({:reviewed, patch2.id, "rvr"}, proj.id)
     assert GitHub.ServerMock.get_state() == %{
       {{:installation, 91}, 14} => %{
         branches: %{
@@ -452,7 +452,7 @@ defmodule BorsNG.BatcherTest do
       pr_xref: 1,
       commit: "N"}
     |> Repo.insert!()
-    Batcher.handle_cast({:reviewed, patch.id}, proj.id)
+    Batcher.handle_cast({:reviewed, patch.id, "rvr"}, proj.id)
     Batcher.handle_info({:poll, :once}, proj.id)
     batch = Repo.get_by! Batch, project_id: proj.id
     assert batch.state == 0

apps/bors_database/lib/link_patch_batch.ex
@@ -12,6 +12,13 @@ defmodule BorsNG.Database.LinkPatchBatch do
   schema "link_patch_batch" do
     belongs_to :patch, Patch
     belongs_to :batch, Batch
+    field :reviewer, :string
+  end
+
+  def from_batch(batch_id) do
+    from l in LinkPatchBatch,
+      preload: [:patch],
+      where: l.batch_id == ^batch_id
   end
 
   @doc """
@@ -19,7 +26,7 @@ defmodule BorsNG.Database.LinkPatchBatch do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:patch_id, :batch_id])
-    |> validate_required([:patch_id, :batch_id])
+    |> cast(params, [:patch_id, :batch_id, :reviewer])
+    |> validate_required([:patch_id, :batch_id, :reviewer])
   end
 end

apps/bors_database/priv/repo/migrations/20170314164114_reviewer.exs
@@ -1,0 +1,9 @@
+defmodule BorsNG.Database.Repo.Migrations.Reviewer do
+  use Ecto.Migration
+
+  def change do
+    alter table(:link_patch_batch) do
+      add :reviewer, :string
+    end
+  end
+end

apps/bors_frontend/config/config.exs
@@ -8,6 +8,7 @@ use Mix.Config
 # General application configuration
 config :bors_frontend, BorsNG,
   activation_phrase: "bors r+",
+  activation_by_phrase: "bors r=",
   deactivation_phrase: "bors r-",
   home_url: "https://bors-ng.github.io/",
   try_phrase: "bors try"

apps/bors_frontend/lib/bors_ng/batcher/message.ex
@@ -71,11 +71,18 @@ defmodule BorsNG.Batcher.Message do
     "#{acc}\n  * #{status_link}"
   end
 
-  def generate_commit_message(patches) do
-    patches = Enum.sort_by(patches, &(&1.pr_xref))
-    commit_title = Enum.reduce(patches, "Merge", &"#{&2} \##{&1.pr_xref}")
-    commit_body = Enum.reduce(patches, "", &"#{&2}#{&1.pr_xref}: #{&1.title}\n")
-    "#{commit_title}\n\n#{commit_body}"
+  def generate_commit_message(patch_links) do
+    patch_links = Enum.sort_by(patch_links, &(&1.patch.pr_xref))
+    commit_title = Enum.reduce(patch_links,
+      "Merge", &"#{&2} \##{&1.patch.pr_xref}")
+    commit_body = Enum.reduce(patch_links, "", fn link, acc ->
+      """
+      #{acc}
+      #{link.patch.pr_xref}: #{link.patch.title} r=#{link.reviewer}
+      #{link.patch.body}
+      """
+    end)
+    "#{commit_title}\n#{commit_body}"
   end
 
   def generate_bors_toml_error(:parse_failed) do

apps/bors_frontend/test/batcher/message_test.exs
@@ -50,13 +50,13 @@ defmodule BorsNG.BatcherMessageTest do
   end
 
   test "generate commit message" do
-    expected_message = "Merge #1 #2\n\n1: Alpha\n2: Beta\n"
+    expected_message = "Merge #1 #2\n\n1: Alpha r=r\na\n\n2: Beta r=s\nb\n"
     patches = [
-      %{pr_xref: 1, title: "Alpha"},
-      %{pr_xref: 2, title: "Beta"}]
+      %{patch: %{pr_xref: 1, title: "Alpha", body: "a"}, reviewer: "r"},
+      %{patch: %{pr_xref: 2, title: "Beta", body: "b"}, reviewer: "s"}]
     patches2 = [
-      %{pr_xref: 2, title: "Beta"},
-      %{pr_xref: 1, title: "Alpha"}]
+      %{patch: %{pr_xref: 2, title: "Beta", body: "b"}, reviewer: "s"},
+      %{patch: %{pr_xref: 1, title: "Alpha", body: "a"}, reviewer: "r"}]
     actual_message = Message.generate_commit_message(patches)
     assert expected_message == actual_message
     actual_message2 = Message.generate_commit_message(patches2)

apps/bors_frontend/test/command_test.exs
@@ -3,6 +3,8 @@ defmodule BorsNG.CommandTest do
 
   alias BorsNG.Command
 
+  doctest BorsNG.Command
+
   test "reject the empty string" do
     assert :nomatch == Command.parse("")
     assert :nomatch == Command.parse(nil)

commit a1557b60b71bb059493d91d1cee172c6f51d2776

Fix crash when trying to bisect a batch
Fixes a crash that happened to badboy, and probably others.

Regression introduced by #121

apps/bors_worker/lib/batcher.ex
@@ -273,7 +273,7 @@ defmodule BorsNG.Worker.Batcher do
   defp start_waiting_merged_batch(batch, patch_links, _base, :conflict) do
     repo_conn = get_repo_conn(batch.project)
     patches = Enum.map(patch_links, &(&1.patch))
-    state = bisect(patches, batch)
+    state = bisect(patch_links, batch)
     send_message(repo_conn, patches, {:conflict, state})
     {:conflict, nil}
   end
@@ -344,19 +344,21 @@ defmodule BorsNG.Worker.Batcher do
     project = batch.project
     repo_conn = get_repo_conn(project)
     erred = Enum.filter(statuses, &(&1.state == Status.numberize_state(:error)))
-    patches = batch.id
-    |> Patch.all_for_batch()
+    patch_links = batch.id
+    |> LinkPatchBatch.from_batch()
     |> Repo.all()
-    state = bisect(patches, batch)
+    patches = Enum.map(patch_links, &(&1.patch))
+    state = bisect(patch_links, batch)
     send_message(repo_conn, patches, {state, erred})
   end
 
   defp timeout_batch(batch) do
     project = batch.project
-    patches = batch.id
-    |> Patch.all_for_batch()
+    patch_links = batch.id
+    |> LinkPatchBatch.from_batch()
     |> Repo.all()
-    state = bisect(patches, batch)
+    patches = Enum.map(patch_links, &(&1.patch))
+    state = bisect(patch_links, batch)
     project
     |> get_repo_conn()
     |> send_message(patches, {:timeout, state})
@@ -379,10 +381,11 @@ defmodule BorsNG.Worker.Batcher do
 
   defp cancel_patch(batch, patch_id, :running) do
     project = batch.project
-    patches = batch.id
-    |> Patch.all_for_batch()
+    patch_links = batch.id
+    |> LinkPatchBatch.from_batch()
     |> Repo.all()
-    state = case tl(patches) do
+    patches = Enum.map(patch_links, &(&1.patch))
+    state = case tl(patch_links) do
       [] -> :failed
       _ -> :retrying
     end
@@ -391,9 +394,9 @@ defmodule BorsNG.Worker.Batcher do
     |> Batch.changeset(%{state: canceled})
     |> Repo.update!()
     if state == :retrying do
-      patches
-      |> Enum.filter(&(&1.id == patch_id))
-      |> make_batch(project.id, batch.into_branch)
+      patch_links
+      |> Enum.filter(&(&1.patch_id == patch_id))
+      |> clone_batch(project.id, batch.into_branch)
     end
     repo_conn = get_repo_conn(project)
     send_status(repo_conn, batch, :canceled)
@@ -414,12 +417,12 @@ defmodule BorsNG.Worker.Batcher do
     send_message(repo_conn, [patch], {:canceled, :failed})
   end
 
-  defp bisect(patches, %Batch{project: project, into_branch: into_branch}) do
-    count = Enum.count(patches)
+  defp bisect(patch_links, %Batch{project: project, into_branch: into_branch}) do
+    count = Enum.count(patch_links)
     if count > 1 do
-      {patches_lo, patches_hi} = Enum.split(patches, div(count, 2))
-      make_batch(patches_lo, project.id, into_branch)
-      make_batch(patches_hi, project.id, into_branch)
+      {lo, hi} = Enum.split(patch_links, div(count, 2))
+      clone_batch(lo, project.id, into_branch)
+      clone_batch(hi, project.id, into_branch)
       :retrying
     else
       :failed
@@ -455,10 +458,10 @@ defmodule BorsNG.Worker.Batcher do
     end
   end
 
-  defp make_batch(patches, project_id, into_branch) do
+  defp clone_batch(patch_links, project_id, into_branch) do
     batch = Repo.insert!(Batch.new(project_id, into_branch))
-    patches
-    |> Enum.map(&%{batch_id: batch.id, patch_id: &1.id})
+    patch_links
+    |> Enum.map(&%{batch_id: batch.id, patch_id: &1.patch_id, reviewer: &1.reviewer})
     |> Enum.map(&LinkPatchBatch.changeset(%LinkPatchBatch{}, &1))
     |> Enum.each(&Repo.insert!/1)
     batch

apps/bors_worker/test/batcher/batcher_test.exs
@@ -493,6 +493,163 @@ defmodule BorsNG.Worker.BatcherTest do
       }}
   end
 
+  test "full runthrough with test failure", %{proj: proj} do
+    # Projects are created with a "waiting" state
+    GitHub.ServerMock.put_state(%{
+      {{:installation, 91}, 14} => %{
+        branches: %{"master" => "ini", "staging" => "", "staging.tmp" => ""},
+        comments: %{1 => [], 2 => []},
+        statuses: %{},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }})
+    patch = %Patch{
+      project_id: proj.id,
+      pr_xref: 1,
+      commit: "N",
+      into_branch: "master"}
+    |> Repo.insert!()
+    patch2 = %Patch{
+      project_id: proj.id,
+      pr_xref: 2,
+      commit: "O",
+      into_branch: "master"}
+    |> Repo.insert!()
+    Batcher.handle_cast({:reviewed, patch.id, "rvr"}, proj.id)
+    Batcher.handle_cast({:reviewed, patch2.id, "rvr"}, proj.id)
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{"master" => "ini", "staging" => "", "staging.tmp" => ""},
+        comments: %{1 => [], 2 => []},
+        statuses: %{"N" => %{"bors" => :running}, "O" => %{"bors" => :running}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    batch = Repo.get_by! Batch, project_id: proj.id
+    assert batch.state == 0
+    # Polling at a later time (yeah, I'm setting the clock back to do it)
+    # kicks it off.
+    batch
+    |> Batch.changeset(%{last_polled: 0})
+    |> Repo.update!()
+    Batcher.handle_info({:poll, :once}, proj.id)
+    batch = Repo.get_by! Batch, project_id: proj.id
+    assert batch.state == 1
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{
+          "master" => "ini",
+          "staging" => "iniNO"},
+        comments: %{1 => [], 2 => []},
+        statuses: %{"N" => %{"bors" => :running}, "O" => %{"bors" => :running}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    # Tell the batcher that the test suite failed.
+    # It should send out an error, and start retrying.
+    Batcher.do_handle_cast({:status, {"iniNO", "ci", :error, nil}}, proj.id)
+    batch = Repo.get! Batch, batch.id
+    assert batch.state == 3
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{
+          "master" => "ini",
+          "staging" => "iniNO"},
+        comments: %{
+          1 => ["# Build failed (retrying...)\n  * ci"],
+          2 => ["# Build failed (retrying...)\n  * ci"]},
+        statuses: %{
+          "iniNO" => %{"bors" => :error},
+          "N" => %{"bors" => :error},
+          "O" => %{"bors" => :error}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    # Kick off the new, replacement batch.
+    [batch_lo, batch_hi] = Repo.all(Batch.all_for_project(proj.id, :waiting))
+    batch_lo
+    |> Batch.changeset(%{last_polled: 0})
+    |> Repo.update!()
+    Batcher.handle_info({:poll, :once}, proj.id)
+    batch_lo = Repo.get_by! Batch, id: batch_lo.id
+    assert batch_lo.state == 1
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{
+          "master" => "ini",
+          "staging" => "iniN"},
+        comments: %{
+          1 => ["# Build failed (retrying...)\n  * ci"],
+          2 => ["# Build failed (retrying...)\n  * ci"]},
+        statuses: %{
+          "iniNO" => %{"bors" => :error},
+          "N" => %{"bors" => :running},
+          "O" => %{"bors" => :error}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    # Tell the batcher that the test suite failed.
+    # It should out an error, and not retry (because the batch has one item in it).
+    Batcher.do_handle_cast({:status, {"iniN", "ci", :error, nil}}, proj.id)
+    batch_lo = Repo.get! Batch, batch_lo.id
+    assert batch_lo.state == 3
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{
+          "master" => "ini",
+          "staging" => "iniN"},
+        comments: %{
+          1 => ["# Build failed\n  * ci", "# Build failed (retrying...)\n  * ci"],
+          2 => ["# Build failed (retrying...)\n  * ci"]},
+        statuses: %{
+          "iniNO" => %{"bors" => :error},
+          "iniN" => %{"bors" => :error},
+          "N" => %{"bors" => :error},
+          "O" => %{"bors" => :error}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    # Kick off the other replacement batch.
+    batch_hi
+    |> Batch.changeset(%{last_polled: 0})
+    |> Repo.update!()
+    Batcher.handle_info({:poll, :once}, proj.id)
+    batch_hi = Repo.get_by! Batch, id: batch_hi.id
+    assert batch_hi.state == 1
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{
+          "master" => "ini",
+          "staging" => "iniO"},
+        comments: %{
+          1 => ["# Build failed\n  * ci", "# Build failed (retrying...)\n  * ci"],
+          2 => ["# Build failed (retrying...)\n  * ci"]},
+        statuses: %{
+          "iniNO" => %{"bors" => :error},
+          "iniN" => %{"bors" => :error},
+          "N" => %{"bors" => :error},
+          "O" => %{"bors" => :running}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    # Tell the batcher that the test suite failed.
+    # It should send out an error, and not retry (because the batch has one item in it).
+    Batcher.do_handle_cast({:status, {"iniO", "ci", :error, nil}}, proj.id)
+    batch_hi = Repo.get! Batch, batch_hi.id
+    assert batch_hi.state == 3
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{
+          "master" => "ini",
+          "staging" => "iniO"},
+        comments: %{
+          1 => ["# Build failed\n  * ci", "# Build failed (retrying...)\n  * ci"],
+          2 => ["# Build failed\n  * ci", "# Build failed (retrying...)\n  * ci"]},
+        statuses: %{
+          "iniNO" => %{"bors" => :error},
+          "iniN" => %{"bors" => :error},
+          "iniO" => %{"bors" => :error},
+          "N" => %{"bors" => :error},
+          "O" => %{"bors" => :error}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    # There should be no more items in the queue now
+    [] = Repo.all(Batch.all_for_project(proj.id, :waiting))
+  end
+
   test "full with differing branches", %{proj: proj} do
     # Projects are created with a "waiting" state
     GitHub.ServerMock.put_state(%{
@@ -578,6 +735,172 @@ defmodule BorsNG.Worker.BatcherTest do
           "O" => %{"bors" => :running}},
         files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
       }}
+  end
+
+  test "full runthrough with test timeout", %{proj: proj} do
+    # Projects are created with a "waiting" state
+    GitHub.ServerMock.put_state(%{
+      {{:installation, 91}, 14} => %{
+        branches: %{"master" => "ini", "staging" => "", "staging.tmp" => ""},
+        comments: %{1 => [], 2 => []},
+        statuses: %{},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }})
+    patch = %Patch{
+      project_id: proj.id,
+      pr_xref: 1,
+      commit: "N",
+      into_branch: "master"}
+    |> Repo.insert!()
+    patch2 = %Patch{
+      project_id: proj.id,
+      pr_xref: 2,
+      commit: "O",
+      into_branch: "master"}
+    |> Repo.insert!()
+    Batcher.handle_cast({:reviewed, patch.id, "rvr"}, proj.id)
+    Batcher.handle_cast({:reviewed, patch2.id, "rvr"}, proj.id)
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{"master" => "ini", "staging" => "", "staging.tmp" => ""},
+        comments: %{1 => [], 2 => []},
+        statuses: %{"N" => %{"bors" => :running}, "O" => %{"bors" => :running}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    batch = Repo.get_by! Batch, project_id: proj.id
+    assert batch.state == 0
+    # Polling at a later time (yeah, I'm setting the clock back to do it)
+    # kicks it off.
+    batch
+    |> Batch.changeset(%{last_polled: 0})
+    |> Repo.update!()
+    Batcher.handle_info({:poll, :once}, proj.id)
+    batch = Repo.get_by! Batch, project_id: proj.id
+    assert batch.state == 1
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{
+          "master" => "ini",
+          "staging" => "iniNO"},
+        comments: %{1 => [], 2 => []},
+        statuses: %{"N" => %{"bors" => :running}, "O" => %{"bors" => :running}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    # Polling at a later time causes the test to time out.
+    # It should send out an error, and start retrying.
+    batch
+    |> Batch.changeset(%{timeout_at: 0})
+    |> Repo.update!()
+    Batcher.handle_info({:poll, :once}, proj.id)
+    batch = Repo.get! Batch, batch.id
+    assert batch.state == 3
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{
+          "master" => "ini",
+          "staging" => "iniNO"},
+        comments: %{
+          1 => ["# Timed out (retrying...)"],
+          2 => ["# Timed out (retrying...)"]},
+        statuses: %{
+          "iniNO" => %{"bors" => :error},
+          "N" => %{"bors" => :error},
+          "O" => %{"bors" => :error}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    # Kick off the new, replacement batch.
+    [batch_lo, batch_hi] = Repo.all(Batch.all_for_project(proj.id, :waiting))
+    batch_lo
+    |> Batch.changeset(%{last_polled: 0})
+    |> Repo.update!()
+    Batcher.handle_info({:poll, :once}, proj.id)
+    batch_lo = Repo.get_by! Batch, id: batch_lo.id
+    assert batch_lo.state == 1
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{
+          "master" => "ini",
+          "staging" => "iniN"},
+        comments: %{
+          1 => ["# Timed out (retrying...)"],
+          2 => ["# Timed out (retrying...)"]},
+        statuses: %{
+          "iniNO" => %{"bors" => :error},
+          "N" => %{"bors" => :running},
+          "O" => %{"bors" => :error}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    # Polling at a later time causes the test to time out.
+    # It should out an error, and not retry (because the batch has one item in it).
+    batch_lo
+    |> Batch.changeset(%{timeout_at: 0})
+    |> Repo.update!()
+    Batcher.handle_info({:poll, :once}, proj.id)
+    batch_lo = Repo.get! Batch, batch_lo.id
+    assert batch_lo.state == 3
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{
+          "master" => "ini",
+          "staging" => "iniN"},
+        comments: %{
+          1 => ["# Timed out", "# Timed out (retrying...)"],
+          2 => ["# Timed out (retrying...)"]},
+        statuses: %{
+          "iniNO" => %{"bors" => :error},
+          "iniN" => %{"bors" => :error},
+          "N" => %{"bors" => :error},
+          "O" => %{"bors" => :error}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    # Kick off the other replacement batch.
+    batch_hi
+    |> Batch.changeset(%{last_polled: 0})
+    |> Repo.update!()
+    Batcher.handle_info({:poll, :once}, proj.id)
+    batch_hi = Repo.get_by! Batch, id: batch_hi.id
+    assert batch_hi.state == 1
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{
+          "master" => "ini",
+          "staging" => "iniO"},
+        comments: %{
+          1 => ["# Timed out", "# Timed out (retrying...)"],
+          2 => ["# Timed out (retrying...)"]},
+        statuses: %{
+          "iniNO" => %{"bors" => :error},
+          "iniN" => %{"bors" => :error},
+          "N" => %{"bors" => :error},
+          "O" => %{"bors" => :running}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    # Polling at a later time causes the test to time out.
+    # It should send out an error, and not retry (because the batch has one item in it).
+    batch_hi
+    |> Batch.changeset(%{timeout_at: 0})
+    |> Repo.update!()
+    Batcher.handle_info({:poll, :once}, proj.id)
+    batch_hi = Repo.get! Batch, batch_hi.id
+    assert batch_hi.state == 3
+    assert GitHub.ServerMock.get_state() == %{
+      {{:installation, 91}, 14} => %{
+        branches: %{
+          "master" => "ini",
+          "staging" => "iniO"},
+        comments: %{
+          1 => ["# Timed out", "# Timed out (retrying...)"],
+          2 => ["# Timed out", "# Timed out (retrying...)"]},
+        statuses: %{
+          "iniNO" => %{"bors" => :error},
+          "iniN" => %{"bors" => :error},
+          "iniO" => %{"bors" => :error},
+          "N" => %{"bors" => :error},
+          "O" => %{"bors" => :error}},
+        files: %{"staging.tmp" => %{"bors.toml" => ~s/status = [ "ci" ]/}}
+      }}
+    # There should be no more items in the queue now
+    [] = Repo.all(Batch.all_for_project(proj.id, :waiting))
   end
 
   test "infer from .travis.yml", %{proj: proj} do