10aef7bd6edd2ed5085f7df6903a7743278a1647
Michael Howell <michael@notriddle.com>
2018-01-04 13:18:16 -0700

    Update to a version of Phoenix that actually has the `init/2` callback

--- apps/bors_frontend/mix.exs
+++ apps/bors_frontend/mix.exs
@@ -30,7 +30,7 @@ defmodule BorsNG.Mixfile do
   # Specifies your project dependencies.
   defp deps do
     [
-      {:phoenix, "~> 1.2.1"},
+      {:phoenix, "~> 1.3.0"},
       {:phoenix_ecto, "~> 3.0"},
       {:phoenix_html, "~> 2.6"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
