dca29788ccaa92feb9a54c9eb624e8e096eda83f
Michael Howell <michael@notriddle.com>
2018-02-13 18:21:19 -0700

    Revert "Update webhook names"
    This reverts commit 151374d6d0de712f8f581519a40d696e60ad05f1.

--- apps/bors_frontend/web/controllers/webhook_controller.ex
+++ apps/bors_frontend/web/controllers/webhook_controller.ex
@@ -91,6 +91,15 @@ defmodule BorsNG.WebhookController do
     :ok
   end
 
+  # 2 hooks for deprecated events, they will be deleted after November 22, 2017
+  def do_webhook(_conn, "github", "integration_installation") do
+    :ok
+  end
+
+  def do_webhook(_conn, "github", "integration_installation_repositories") do
+    :ok
+  end
+
   def do_webhook(conn, "github", "installation") do
     payload = conn.body_params
     installation_xref = payload["installation"]["id"]
@@ -110,7 +119,7 @@ defmodule BorsNG.WebhookController do
     :ok
   end
 
-  def do_webhook(conn, "github", "repositories") do
+  def do_webhook(conn, "github", "installation_repositories") do
     payload = conn.body_params
     installation_xref = payload["installation"]["id"]
     installation = Repo.get_by!(
