76d46a2fd92bdef660e143317211b7c99696d5d9
Michael Howell <michael@notriddle.com>
2019-07-05 15:22:02 -0700

    Switch from brunch to webpack
    Brunch isn't really being maintained any more,
    and some of its deps are giving me security warnings.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,10 @@
-// Brunch automatically concatenates all files in your
+// We need to import the CSS so that webpack will load it.
+// The MiniCssExtractPlugin is used to separate it out into
+// its own CSS file.
+import normalize_css from "../css/normalize.css"
+import css from "../css/app.css"
+
+// Webpack automatically concatenates all files in your
 // watched paths. Those paths can be configured at
 // config.paths.watched in "brunch-config.js".
 //
