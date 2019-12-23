3b95af526a272c1acbb5b776613afaf48f7c3db4
Adam Neumann <adam.neumann@gusto.com>
2019-12-23 13:57:50 -0700

    Update view to fit bors look and feel

--- lib/web/views/batch_view.ex
+++ lib/web/views/batch_view.ex
@@ -4,4 +4,15 @@ defmodule BorsNG.BatchView do
   """
 
   use BorsNG.Web, :view
+
+  def stringify_state(state) do
+    case state do
+      :waiting  -> "Waiting to run"
+      :running  -> "Running"
+      :ok       -> "Succeeded"
+      :error    -> "Failed"
+      :canceled -> "Canceled"
+      _         -> "Invalid"
+    end
+  end
 end
