78c4cb0e679430a93dec12e43910face484784cf
Michael Howell <michael@notriddle.com>
2019-12-28 10:14:34 -0700

    Add navigation aids and date to batch page

--- lib/web/views/batch_view.ex
+++ lib/web/views/batch_view.ex
@@ -15,4 +15,11 @@ defmodule BorsNG.BatchView do
       _         -> "Invalid"
     end
   end
+
+  def htmlify_naive_datetime(datetime) do
+    [ "<td><time class=time-convert>",
+      NaiveDateTime.to_iso8601(datetime),
+      "+00:00</time></td>" ]
+    |> Phoenix.HTML.raw()
+  end
 end
