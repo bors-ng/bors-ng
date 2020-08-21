288caf8c5d9b8066b1e68cf122e188ed435fe92b
Michael Howell <michael@notriddle.com>
2020-08-21 15:49:16 -0700

    Update webpack dependencies, including breaking copy-plugin changes

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -39,6 +39,6 @@ module.exports = (env, options) => ({
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
+    new CopyWebpackPlugin({ patterns: [ { from: 'static/', to: '../' } ] })
   ]
 });
