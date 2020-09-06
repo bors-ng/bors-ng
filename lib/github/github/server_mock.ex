c8ad9ba69ac7317caeedd7df7145f5e218a2dd8d
Juan Pablo Civile <elementohb@gmail.com>
2020-09-06 11:53:24 -0300

    enable using update_pr in Github API mocks

--- lib/github/github/server_mock.ex
+++ lib/github/github/server_mock.ex
@@ -160,6 +160,20 @@ defmodule BorsNG.GitHub.ServerMock do
     end
   end
 
+  def do_handle_call(:update_pr, repo_conn, pr, state) do
+    with {:ok, repo} <- Map.fetch(state, repo_conn),
+         {:ok, pulls} <- Map.fetch(repo, :pulls) do
+      pulls = %{pulls | pr.number => pr}
+      repo = %{repo | pulls: pulls}
+      state = %{state | repo_conn => repo}
+      {{:ok, pr}, state}
+    end
+    |> case do
+      {{:ok, _}, _} = res -> res
+      _ -> {{:error, :update_pr}, state}
+    end
+  end
+
   def do_handle_call(:get_pr_files, repo_conn, {_pr_xref}, state) do
     files =
       with {:ok, repo} <- Map.fetch(state, repo_conn),
