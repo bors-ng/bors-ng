351da030c3ea6c6815d4fe21be1639a1a1601f60
Michael Howell <michael@notriddle.com>
2022-02-01 11:24:44 -0700

    Stop using deprecated macro-mode Phoenix.ConnTest, instead import

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -18,7 +18,8 @@ defmodule BorsNG.ConnCase do
   using do
     quote do
       # Import conveniences for testing with connections
-      use Phoenix.ConnTest
+      import Plug.Conn
+      import Phoenix.ConnTest
 
       alias BorsNG.Database
       import Ecto
