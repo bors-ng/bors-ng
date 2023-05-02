ef02a1388dfa1b35b540717ce3ab83cb04eec5f3
Michael Howell <michael@notriddle.com>
2023-05-01 17:20:50 -0700

    Use a working adapter for http in oauth2

--- config/prod.exs
+++ config/prod.exs
@@ -2,5 +2,6 @@ import Config
 
 config :bors, :server, BorsNG.GitHub.Server
 config :bors, :oauth2, BorsNG.GitHub.OAuth2
+config :oauth2, adapter: Tesla.Adapter.Hackney
 
 import_config "prod.secret.exs"
